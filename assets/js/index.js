8f3b390ac7778749312c2b4fa61fa302663bf5c4
julroubens <julroubens2005@gmail.com>
2023-04-24 12:44:20 -0400

    email sender good

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,26 +1,18 @@
 function sendEmail() {
+   var name = document.getElementById("name").value;
+   var email = document.getElementById("email").value;
+   var subject = document.getElementById("subject").value;
+   var message1 = document.getElementById("message").value;
+   
     Email.send({
-        Host : "sandbox.smtp.mailtrap.io",
-        Username : "adf7afeb170dcc",
-        Password : "23b28d69ed2141",
-        To : 'julroubens2005@gmail.com',
-        From : "rougens.jules@esih.edu",
-        Subject : "Test email",
-        Body : "<html><h2>Header</h2><strong>Bold text</strong><br></br><em>Italic</em></html>"
+        Host : "smtp.elasticemail.com",
+        Username : "julroubens2005@gmail.com",
+        Password : "7363E9F71838F0714103904643A182C1BDC3",
+        To : email,
+        From : "julroubens2005@gmail.com",
+        Subject : subject,
+        Body : name+"\n"+message1
       }).then(
         message => alert(message)
       );
-      console.log('FAILED...', "ok");
-   // var templateParams = {
-   //    name: 'James',
-   //    notes: 'Check this out!'
-   // };
-
-
-   // emailjs.send('service_7gbgr5d', 'YOUR_TEMPLATE_ID', templateParams) //use your Service ID and Template ID
-   //    .then(function(response) {
-   //       console.log('SUCCESS!', response.status, response.text);
-   //    }, function(error) {
-   //       console.log('FAILED...', error);
-   //    });
 }
